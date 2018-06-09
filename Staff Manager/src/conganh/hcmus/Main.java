89ebeb5bcac3e9b9d9d69864a60c98d95279a8d9
Lê Đoàn Công Ảnh <conganhhcmus@gmail.com>
2018-06-09 10:05:12 +0700

    Staff Manager

--- Staff Manager/src/conganh/hcmus/Main.java
+++ Staff Manager/src/conganh/hcmus/Main.java
@@ -1,17 +1,19 @@
 package conganh.hcmus;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Scanner;
 
 public class Main {
+    private static final String FILE_NAME = "Staff_Manager.dat";
     private static ArrayList<Cadres> listCadres = new ArrayList<Cadres>();
     public static void main(String[] args) {
         while(menu()) ;
-
     }
     private static boolean menu() {
         boolean check = true;
+       // readFile();
         System.out.println("Cadres Manager: ");
         System.out.println("1. Input/Enter the Cadres");
         System.out.println("2. Search by Name");
@@ -33,6 +35,7 @@ public class Main {
                 System.err.println("Exit!");
                 break;
         }
+       // writeFile();
         return check;
     }
 
@@ -109,8 +112,41 @@ public class Main {
 
     private static void outputAllCadres() {
         Iterator itr = listCadres.iterator();
-        while (itr.hasNext()) {
-            System.out.println(itr.next());
+        if (!itr.hasNext()) {
+            System.out.println("List is empty !");
+        } else {
+            while (itr.hasNext()) {
+                System.out.println(itr.next());
+            }
+        }
+    }
+
+    private static void readFile() {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(FILE_NAME);
+            BufferedInputStream file = new BufferedInputStream(fileInputStream);
+            System.out.println("Successful!");
+        } catch (Exception ex) {
+            System.err.println("Error");
+            ex.printStackTrace();
+        }
+    }
+
+    private  static void writeFile() {
+        FileOutputStream fileOutputStream = null;
+        try {
+            fileOutputStream = new FileOutputStream(FILE_NAME);
+            BufferedOutputStream file = new BufferedOutputStream(fileOutputStream);
+            Iterator itr = listCadres.iterator();
+            while (itr.hasNext()) {
+                Cadres tmp = (Cadres) itr.next();
+                file.write(tmp.toString().getBytes());
+            }
+            System.out.println("Successful!");
+        } catch (Exception ex) {
+            System.err.println("Error");
+            ex.printStackTrace();
         }
     }
 }
