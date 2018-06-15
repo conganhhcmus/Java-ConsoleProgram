17b3b5b70f1dead25658a91de4173616b5c4d417
Lê Đoàn Công Ảnh <conganhhcmus@gmail.com>
2018-06-15 21:53:18 +0700

    University Exam

--- University Exam/src/conganh/hcmus/Main.java
+++ University Exam/src/conganh/hcmus/Main.java
@@ -3,6 +3,5 @@ package conganh.hcmus;
 public class Main {
     public static void main(String[] args) {
         System.out.println("University Exam !");
-        System.out.println("Cong anh");
     }
 }
